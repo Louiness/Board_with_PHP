0a0a20adeb188d9a04758a9fd267104129a1d10d
Louiness <kks518@naver.com>
2020-03-05 00:09:45 +0900

    commit

--- Board_PHP/page/board/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "../../db.php";
-
-	$bno = $_GET['idx'];
-	$sql = mq("delete from board where idx='$bno';");
-	echo "<script>alert('削除なりました.');</script>";
-?>
-<meta http-equiv="refresh" content="0 url=/board.php" />
